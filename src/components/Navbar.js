b3bd932b20b4841697ceb9b6df1d9b7589e0925c
Tanmay Jauhari <www.tanmayjauhari@gmail.com>
2023-02-20 20:37:34 +0530

    first commit

---
+++ src/components/Navbar.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { useSelector } from 'react-redux';
+
+const Navbar = () => {
+    const items = useSelector((state) => state.cart);
+
+    return (
+        <div style={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center' }}>
+            <span className='logo'>REDUX STORE🛒🛍️</span>
+            <div>
+                <Link to='/' className='navLink'>Home</Link>
+                <Link to='/cart' className='navLink'>Cart</Link>
+                <span className='cartCount'>
+                    Cart items: {items.length}
+                </span>
+            </div>
+        </div>
+    )
+}
+
+export default Navbar;
